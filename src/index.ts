b3fc5550d76be98ed8c0763db37e8f6d69e88b1c
Anton <akushch@twilio.com>
2021-04-28 15:59:47 +0200

    works

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import * as FlexPlugin from 'flex-plugin';
-import AgentAutoresponsePlugin from "./cannedResponse/AgentAutoresponsePlugin";
+import AgentAutoresponsePlugin from "./AgentAutoresponsePlugin";
 
 FlexPlugin.loadPlugin(AgentAutoresponsePlugin);
